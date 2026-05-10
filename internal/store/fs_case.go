ef85f2ae0e6dffc911fa35bcfd6d8adb0887fde9
David Koosis <dkoosis@gmail.com>
2026-05-10 04:24:27 -0500

    feat(loto-v2): cached fs case-sensitivity probe

---
+++ internal/store/fs_case.go
@@ -0,0 +1,46 @@
+package store
+
+import (
+	"database/sql"
+	"errors"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+func (s *Store) FSCaseSensitive(probeDir string) (bool, error) {
+	const key = "fs_case_sensitive"
+	var v string
+	err := s.db.QueryRow(`SELECT value FROM schema_meta WHERE key = ?`, key).Scan(&v)
+	if err == nil {
+		b, perr := strconv.ParseBool(v)
+		return b, perr
+	}
+	if !errors.Is(err, sql.ErrNoRows) {
+		return false, err
+	}
+	probed, err := probeFSCase(probeDir)
+	if err != nil {
+		return false, err
+	}
+	_, err = s.db.Exec(`INSERT OR REPLACE INTO schema_meta(key,value) VALUES (?,?)`, key, strconv.FormatBool(probed))
+	return probed, err
+}
+
+func probeFSCase(dir string) (bool, error) {
+	low := filepath.Join(dir, ".loto-case-probe")
+	up := filepath.Join(dir, ".LOTO-CASE-PROBE")
+	if err := os.WriteFile(low, []byte("x"), 0o600); err != nil {
+		return false, err
+	}
+	defer os.Remove(low)
+	stLow, err := os.Stat(low)
+	if err != nil {
+		return false, err
+	}
+	stUp, err := os.Stat(up)
+	if err != nil {
+		return true, nil
+	}
+	return !os.SameFile(stLow, stUp), nil
+}
